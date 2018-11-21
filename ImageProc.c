deeca06701db6ea9fcba66fdbf7d27707f6f204b
chenweihao <chenweihao@flyaudio.cn>
2018-11-21 15:52:27 +0800

    基本完成

--- ImageProc.c
+++ ImageProc.c
@@ -1,33 +1,542 @@
 #include "ImageProc.h"
 
 int openDevice(char* dev) {
+	struct stat st;
 	int length = sizeof(dev)/sizeof(char);
 	if (length == 0) {
-		/* code */
+		ALOGE("error param %s", dev);
 	}
-	if (-1 == stat()) {
-		/* code */
+	if (-1 == stat(dev, &st)) {
+		ALOGE("open device %s failed, dev is not exist", dev);
+	}
+	fd = open(dev, O_RDWR| O_NONBLOCK, 0);
+	if (fd == -1) {
+		ALOGE("open device %s failed", dev);
 	}
 	return 0;
 }
 
-int getCapability(char* dev) {
-	return 0;
+int getCapability(int fd) {
+	struct v4l2_capability capability;
+	int ret = 0;
+	ret = xioctl(fd, VIDIOC_QUERYCAP, &capability);
+	if (ret == -1) {
+		ALOGE("not support querycap");
+	} else {
+		ALOGE("V4L2_CAP_VIDEO_CAPTURE %d", capability.capabilities&V4L2_CAP_VIDEO_CAPTURE);
+		//ALOGE("V4L2_CAP_TUNER %d", capability.capabilities&V4L2_CAP_TUNER);
+		//ALOGE("V4L2_CAP_AUDIO %d", capability.capabilities&V4L2_CAP_AUDIO);
+		ALOGE("V4L2_CAP_STREAMING %d", capability.capabilities&V4L2_CAP_STREAMING);
+	}
+	return ret;
 }
 
-int setVideoFmt() {
+int setCropCap(int fd) {
+	int ret = 0;
+	struct v4l2_cropcap cropcap;
+	struct v4l2_crop crop;
+	CLEAR(cropcap);
+	cropcap.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	ret = xioctl(fd, VIDIOC_CROPCAP, &cropcap);
+	if (ret == -1) {
+		ALOGE("VIDIOC_CROPCAP error %d, %s", error, strerror(error));
+	} else if (ret == 0) {
+		ALOGE("cropcap type %d", cropcap.type);
+		ALOGE("cropcap bounds rect left %d", cropcap.bounds.left);
+		ALOGE("cropcap bounds rect top %d", cropcap.bounds.top);
+		ALOGE("cropcap bounds rect width %d", cropcap.bounds.width);
+		ALOGE("cropcap bounds rect height %d", cropcap.bounds.height);
+
+		ALOGE("cropcap defrect rect left %d", cropcap.defrect.left);
+		ALOGE("cropcap defrect rect top %d", cropcap.defrect.top);
+		ALOGE("cropcap defrect rect width %d", cropcap.defrect.width);
+		ALOGE("cropcap defrect rect height %d", cropcap.defrect.height);
+
+		IMG_WIDTH = cropcap.defrect.width;
+		IMG_HEIGHT = cropcap.defrect.height;
+
+		crop.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		crop.c = cropcap.defrect;
+		ret = xioctl(fd, VIDIOC_S_CROP, &crop);
+		if (ret == -1)
+		{
+			ALOGE("setCropCap  failed %d, %s", error, strerror(error));
+		}
+		else {
+			ALOGE("setCropCap  success");
+		}
+	} else {
+		ALOGE("cropcap else");
+	}
+
+	return ret;
+}
+
+int setVideoFmt(int fd) {
+	struct v4l2_format fmt;
+	int min;
+	int ret;
+
+	fmt.type = V4L2_CAP_VIDEO_CAPTURE;
+	fmt.fmt.pix.width = IMG_WIDTH;
+	fmt.fmt.pix.height = IMG_HEIGHT;
+	fmt.fmt.pix.pixelformat = V4L2_PIX_FMT_YUYV;
+	fmt.fmt.pix.field = V4L2_FIELD_INTERLACED;
+
+	ret = xioctl(fd, VIDIOC_S_FMT, &fmt);
+	if (ret == -1)
+	{
+		ALOGE("setVideoFmt failed");
+		return ret;
+	} else{
+		ALOGE("setVideoFmt success");
+	}
+
+	min = fmt.fmt.pix.width * 2;
+	if (fmt.fmt.pix.bytesperline < min)
+	{
+		fmt.fmt.pix.bytesperline = min;
+	}
+	min = fmt.fmt.pix.bytesperline * fmt.fmt.pix.height;
+	if (fmt.fmt.pix.sizeimage < min)
+	{
+		fmt.fmt.pix.sizeimage = min;
+	}
+
+	return ret;
+}
+
+int queryBuffers(int fd) {
+	struct v4l2_requestbuffers req;
+	CLEAR(req);
+	req.count = 4;
+	req.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	req.memory = V4L2_MEMORY_MMAP;
+	if (-1 == xioctl(fd, VIDIOC_REQBUFS, &req))
+	{
+		ALOGE("VIDIOC_REQBUFS failed %d, %s", error, strerror(error));
+		return -1;
+	}
+	if (req.count < 2)
+	{
+		ALOGE("VIDIOC_REQBUFS count is  %d", req.count);
+		return -1;
+	}
+
+	buffers = NULL;
+	buffers = calloc(req.count, sizeof(*buffers));
+	for (n_buffers = 0; n_buffers < req.count; ++n_buffers)
+	{
+		struct v4l2_buffer buf;
+		CLEAR(buf);
+
+		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		buf.memory = V4L2_MEMORY_MMAP;
+		buf.index = n_buffers;
+		if (-1 == xioctl(fd, VIDIOC_QUERYBUF, &buf)) {
+			ALOGE("VIDIOC_REQBUF failed");
+			return -1;
+		}
+		buffers[n_buffers].length = buf.length;
+		buffers[n_buffers].start = mmap(NULL, buf.length, PROT_READ | PROT_WRITE, MAP_SHARED, fd, buf.m.offset);
+		if (MAP_FAILED == buffers[n_buffers].start) {
+			ALOGE("mmap failed ");
+			return -1;
+		}
+	}
 	return 0;
 }
 
 int startStream() {
+	unsigned int i;
+	enum v4l2_buf_type type;
+	for (i = 0; i < n_buffers; ++i)
+	{
+		struct v4l2_buffer buf;
+		CLEAR(buf);
+		buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+		buf.memory = V4L2_MEMORY_MMAP;
+		buf.index = i;
+		if (-1 == xioctl(fd, VIDIOC_QBUF, &buf)) {
+			ALOGE("V4L2_QBUF failed");
+			return -1;
+		}
+	}
+	type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	if (-1 == xioctl(fd, VIDIOC_STREAMON, &type)) {
+		ALOGE("VIDIOC_STREAMON failed %d, %s", error, strerror(error));
+		return -1;
+	}
 	return 0;
 } 
 
 int stopStream() {
+	enum v4l2_buf_type type;
+
+	type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+
+	if (-1 == xioctl (fd, VIDIOC_STREAMOFF, &type))
+		return -1;
+
 	return 0;
 } 
 
-int closeDevice(char* dev) {
+int closeDevice() {
+	if (-1 == close (fd)){
+		fd = -1;
+		ALOGE("close fialed");
+	}
+
+	fd = -1;
 	return 0;
+}
+
+void uninitdevice() {
+	unsigned int i;
+
+	for (i = 0; i < n_buffers; ++i)
+		if (-1 == munmap (buffers[i].start, buffers[i].length))
+			return -1;
+
+	free (buffers);
+
+	return 0;
+}
+
+int readframeonce(void) {
+	for (;;) {
+		fd_set fds;
+		struct timeval tv;
+		int r;
+
+		FD_ZERO (&fds);
+		FD_SET (fd, &fds);
+
+		tv.tv_sec = 2;
+		tv.tv_usec = 0;
+
+		r = select (fd + 1, &fds, NULL, NULL, &tv);
+
+		if (-1 == r) {
+			if (EINTR == errno)
+				continue;
+
+			ALOGE("select failed");
+			return -1;
+		}
+
+		if (0 == r) {
+			ALOGE("select timeout");
+			return -1;
+		}
+
+		if (readframe ()==1);
+		break;
+	}
+
+	return 0;
+}
+
+int readframe(void) {
+	struct v4l2_buffer buf;
+	CLEAR(buf);
+	buf.type = V4L2_BUF_TYPE_VIDEO_CAPTURE;
+	buf.memory = V4L2_MEMORY_MMAP;
+	if (-1 == xioctl(fd, VIDIOC_DQBUF, &buf))
+	{
+		ALOGE("VIDIOC_DQBUF failed");
+		return -1;
+	}
+	assert(buf.index < n_buffers);
+	processimage(buffers[buf.index].start);
+	if (-1 == xioctl(fd, VIDIOC_QBUF, &buf))
+	{
+		ALOGE("VIDIOC_QBUF after VIDIOC_DQBUF failed");
+		return -1;
+	}
+	ALOGE("readframe success");
+	return 1;
+}
+
+void processimage(void* start) {
+	yuyv422toABGRY((unsigned char*)start);
+}
+
+int errorexit() {
+	return 0;
+}
+
+int xioctl(int fd, int request, void* arg) {
+	int ret;
+	ret = ioctl(fd, request, arg);
+	return ret;
+}
+
+void yuyv422toABGRY(unsigned char *src)
+{
+
+	int width=0;
+	int height=0;
+
+	width = IMG_WIDTH;
+	height = IMG_HEIGHT;
+
+	int frameSize =width*height*2;
+
+	int i;
+
+	if((!rgb || !ybuf)){
+		return;
+	}
+	int *lrgb = NULL;
+	int *lybuf = NULL;
+		
+	lrgb = &rgb[0];
+	lybuf = &ybuf[0];
+
+	if(yuv_tbl_ready==0){
+		for(i=0 ; i<256 ; i++){
+			y1192_tbl[i] = 1192*(i-16);
+			if(y1192_tbl[i]<0){
+				y1192_tbl[i]=0;
+			}
+
+			v1634_tbl[i] = 1634*(i-128);
+			v833_tbl[i] = 833*(i-128);
+			u400_tbl[i] = 400*(i-128);
+			u2066_tbl[i] = 2066*(i-128);
+		}
+		yuv_tbl_ready=1;
+	}
+
+	for(i=0 ; i<frameSize ; i+=4){
+		unsigned char y1, y2, u, v;
+		y1 = src[i];
+		u = src[i+1];
+		y2 = src[i+2];
+		v = src[i+3];
+
+		int y1192_1=y1192_tbl[y1];
+		int r1 = (y1192_1 + v1634_tbl[v])>>10;
+		int g1 = (y1192_1 - v833_tbl[v] - u400_tbl[u])>>10;
+		int b1 = (y1192_1 + u2066_tbl[u])>>10;
+
+		int y1192_2=y1192_tbl[y2];
+		int r2 = (y1192_2 + v1634_tbl[v])>>10;
+		int g2 = (y1192_2 - v833_tbl[v] - u400_tbl[u])>>10;
+		int b2 = (y1192_2 + u2066_tbl[u])>>10;
+
+		r1 = r1>255 ? 255 : r1<0 ? 0 : r1;
+		g1 = g1>255 ? 255 : g1<0 ? 0 : g1;
+		b1 = b1>255 ? 255 : b1<0 ? 0 : b1;
+		r2 = r2>255 ? 255 : r2<0 ? 0 : r2;
+		g2 = g2>255 ? 255 : g2<0 ? 0 : g2;
+		b2 = b2>255 ? 255 : b2<0 ? 0 : b2;
+
+		*lrgb++ = 0xff000000 | b1<<16 | g1<<8 | r1;
+		*lrgb++ = 0xff000000 | b2<<16 | g2<<8 | r2;
+
+		if(lybuf!=NULL){
+			*lybuf++ = y1;
+			*lybuf++ = y2;
+		}
+	}
+
+}
+
+int getBrightness() {
+	ctrl.id=BRIGHTNESS_ID;
+    	if(ioctl(fd,VIDIOC_G_CTRL,&ctrl)==-1)
+    	{
+        ALOGE("getBrightness failed");
+        return -1;
+    	} else{
+    		ALOGE("getBrightness success brightness is %d", ctrl.value);
+    	}
+    	return ctrl.value;
+}
+
+int setBrightness(int value) {
+	ctrl.id = BRIGHTNESS_ID;
+	ctrl.value = value;
+	if(ioctl(fd,VIDIOC_S_CTRL,&ctrl)==-1)
+    	{
+        ALOGE("setBrightness failed");
+        return -1;
+    	} else{
+    		ALOGE("setBrightness success brightness is %d", value);
+    	}
+	return 1;
+}
+
+int getContrast() {
+	ctrl.id=CONTRAST_ID;
+    	if(ioctl(fd,VIDIOC_G_CTRL,&ctrl)==-1)
+    	{
+        ALOGE("getContrast failed");
+        return -1;
+    	} else{
+    		ALOGE("getContrast success contrast is %d", ctrl.value);
+    	}
+    	return ctrl.value;
+}
+
+int setContrast(int contrast) {
+	ctrl.id = CONTRAST_ID;
+	ctrl.value = contrast;
+	if(ioctl(fd,VIDIOC_S_CTRL,&ctrl)==-1)
+    	{
+        ALOGE("setContrast failed");
+        return -1;
+    	} else{
+    		ALOGE("setContrast success contrast is %d", contrast);
+    	}
+	return 1;
+}
+
+void 
+Java_com_camera_simplewebcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,jobject bitmap){
+
+	jboolean bo;
+
+
+	AndroidBitmapInfo  info;
+	void*              pixels;
+	int                ret;
+	int i;
+	int *colors;
+
+	int width=0;
+	int height=0;
+
+	if ((ret = AndroidBitmap_getInfo(env, bitmap, &info)) < 0) {
+		ALOGE("AndroidBitmap_getInfo() failed ! error=%d", ret);
+		return;
+	}
+    
+	width = info.width;
+	height = info.height;
+
+	if(!rgb || !ybuf) return;
+
+	if (info.format != ANDROID_BITMAP_FORMAT_RGBA_8888) {
+		ALOGE("Bitmap format is not RGBA_8888 !");
+		return;
+	}
+
+	if ((ret = AndroidBitmap_lockPixels(env, bitmap, &pixels)) < 0) {
+		ALOGE("AndroidBitmap_lockPixels() failed ! error=%d", ret);
+	}
+
+	colors = (int*)pixels;
+	int *lrgb =NULL;
+	lrgb = &rgb[0];
+
+	for(i=0 ; i<width*height ; i++){
+		*colors++ = *lrgb++;
+	}
+
+	AndroidBitmap_unlockPixels(env, bitmap);
+
+}
+
+jint 
+Java_com_camera_simplewebcam_CameraPreview_prepareCamera( JNIEnv* env,jobject thiz, jint videoid){
+	ALOGE("opendevice");
+	char devName[16] = "/dev/video21";
+	if (videoid != 0)
+	{
+		sprintf(devName,"/dev/video%d", videoid);
+	} else {
+		ALOGE("videoid is 0");
+	}
+	openDevice(devName);
+	getCapability(fd);
+	setCropCap(fd);
+	setVideoFmt(fd);
+	queryBuffers(fd);
+	startStream();
+	rgb = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
+	ybuf = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
+	return 0;
+
+	// int ret;
+
+	// if(camerabase<0){
+	// 	camerabase = checkCamerabase();
+	// }
+
+	// ret = opendevice(camerabase + videoid);
+
+	// if(ret != ERROR_LOCAL){
+	// 	ret = initdevice();
+	// }
+	// if(ret != ERROR_LOCAL){
+	// 	ret = startcapturing();
+
+	// 	if(ret != SUCCESS_LOCAL){
+	// 		stopcapturing();
+	// 		uninitdevice ();
+	// 		closedevice ();
+	// 		LOGE("device resetted");	
+	// 	}
+
+	// }
+
+	// if(ret != ERROR_LOCAL){
+	// 	rgb = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
+	// 	ybuf = (int *)malloc(sizeof(int) * (IMG_WIDTH*IMG_HEIGHT));
+	// }
+	// return ret;
+}	
+
+
+
+jint 
+Java_com_camera_simplewebcam_CameraPreview_prepareCameraWithBase( JNIEnv* env,jobject thiz, jint videoid, jint videobase){
+		int ret;
+
+		// camerabase = videobase;
+	
+		return Java_com_camera_simplewebcam_CameraPreview_prepareCamera(env,thiz,videoid);
+	
+}
+
+void 
+Java_com_camera_simplewebcam_CameraPreview_processCamera( JNIEnv* env,
+										jobject thiz){
+
+	readframeonce();
+}
+
+void 
+Java_com_camera_simplewebcam_CameraPreview_stopCamera(JNIEnv* env,jobject thiz){
+
+	stopStream ();
+
+	uninitdevice ();
+
+	closeDevice ();
+
+	if(rgb) free(rgb);
+	if(ybuf) free(ybuf);
+        
+	fd = -1;
+}
+
+jint Java_com_camera_simplewebcam_CameraPreview_getBrightness( JNIEnv* env,jobject thiz) {
+	return getBrightness();
+}
+
+jint Java_com_camera_simplewebcam_CameraPreview_setBrightness( JNIEnv* env,jobject thiz, jint brightness) {
+	return setBrightness(brightness);
+}
+
+jint Java_com_camera_simplewebcam_CameraPreview_getContrast( JNIEnv* env,jobject thiz) {
+	return getContrast();
+}
+
+jint Java_com_camera_simplewebcam_CameraPreview_setContrast( JNIEnv* env,jobject thiz, jint contrast) {
+	return setContrast(contrast);
 }
 
--- ImageProc.h
+++ ImageProc.h
@@ -1,14 +1,88 @@
 #include <stdio.h>
 #include <string.h>
+#include <utils/Log.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <jni.h>
+#include <error.h>
+#include <assert.h>
+#include <errno.h>
+#include <android/bitmap.h>
+
+#include <sys/mman.h>
+
+
+#include <linux/videodev2.h>
+#include <linux/usbdevice_fs.h>
+
 
 #define LOG_TAG 	"WebCam"
-#define LOGE(...) 	__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
-#define LOGI(...) 	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+// #define LOGE(...) 	__android_log_print(ANDROID_LOG_ERROR, LOG_TAG, __VA_ARGS__)
+// #define LOGI(...) 	__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__)
+#define CLEAR(x) memset (&(x), 0, sizeof (x))
+
+#define ERROR_LOCAL -1
+#define SUCCESS_LOCAL 0
+
+static int IMG_WIDTH = 640;
+static int  IMG_HEIGHT = 480;
+
+#define BRIGHTNESS_ID 0x00980900
+#define CONTRAST_ID 0x00980901
+#define SATURATION_ID 0x00980902
+#define HUE_ID 0x00980903
+#define WHITE_BALANCE_TEMP_AUTO_ID 0x0098090c
+#define GAMMA_ID 0x00980910
+#define POWER_LINE_FREQUENCY_ID 0x00980918
+#define WHITE_BALANCE_TEMP_ID 0x0098091a
+#define SHARPNESS_ID 0x0098091b
+#define BACKLIGHT_COMPENSATION_ID 0x0098091c
+#define EXPOSURE_AUTO_ID 0x009a0901
+#define EXPOSURE_ABSOLUTE_ID 0x009a0902
+#define EXPOSURE_AUTO_PRIORITY_ID 0x009a0903
 
 int openDevice(char* dev);
-int getCapability(char* dev);
-int setVideoFmt();
+int getCapability(int fd);
+int setCropCap(int fd);
+int setVideoFmt(int fd);
+int queryBuffers(int fd);
 int startStream();
+int readframe(void);
 int stopStream();
-int closeDevice(char* dev);
+int closeDevice();
+void uninitdevice();
+int getBrightness();
+
+void yuyv422toABGRY(unsigned char *src);
+
+static fd = -1;
 char* devPath;
+struct buffer
+{
+	void* start;
+	size_t length;
+};
+struct buffer* buffers = NULL;
+static size_t n_buffers = 0;
+struct v4l2_control ctrl;
+
+int *rgb = NULL;
+int *ybuf = NULL;
+
+int yuv_tbl_ready=0;
+int y1192_tbl[256];
+int v1634_tbl[256];
+int v833_tbl[256];
+int u400_tbl[256];
+int u2066_tbl[256];
+
+jint Java_com_camera_simplewebcam_CameraPreview_prepareCamera( JNIEnv* env,jobject thiz, jint videoid);
+jint Java_com_camera_simplewebcam_CameraPreview_prepareCameraWithBase( JNIEnv* env,jobject thiz, jint videoid, jint videobase);
+void Java_com_camera_simplewebcam_CameraPreview_processCamera( JNIEnv* env,jobject thiz);
+void Java_com_camera_simplewebcam_CameraPreview_stopCamera(JNIEnv* env,jobject thiz);
+void Java_com_camera_simplewebcam_CameraPreview_pixeltobmp( JNIEnv* env,jobject thiz,jobject bitmap);
+jint Java_com_camera_simplewebcam_CameraPreview_getBrightness( JNIEnv* env,jobject thiz);
+jint Java_com_camera_simplewebcam_CameraPreview_setBrightness( JNIEnv* env,jobject thiz, jint brightness);
+jint Java_com_camera_simplewebcam_CameraPreview_getContrast( JNIEnv* env,jobject thiz);
+jint Java_com_camera_simplewebcam_CameraPreview_setContrast( JNIEnv* env,jobject thiz, jint contrast);
